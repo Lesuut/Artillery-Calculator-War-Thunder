0c66274b54cb294c2ed0cf0b4bea2716f8317c28
Lesut <artem.chaykin.2005@gmail.com>
2024-02-18 15:58:42 +0200

    Add project files.

---
+++ ArtCalculator/Form1.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace ArtCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 450);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.Color.DarkOrange;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(707, 385);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Screen Shot";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(729, 415);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(59, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "999";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(542, 418);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Grid Size:";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(632, 418);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Grid size in map";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(626, 385);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Set Grid";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(542, 385);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Clear Lines";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 388);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Line Distance:";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 418);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Distance to target:";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label5.Location = new System.Drawing.Point(112, 410);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 31);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "___";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(360, 385);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(59, 20);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.Text = "3";
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(318, 388);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "height";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(435, 388);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(32, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "width";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox3.Location = new System.Drawing.Point(477, 385);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(59, 20);
+            this.textBox3.TabIndex = 13;
+            this.textBox3.Text = "3";
+            // 
+            // label8
+            // 
+            this.label8.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(435, 415);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(32, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "zoom";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox4.Location = new System.Drawing.Point(477, 412);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(59, 20);
+            this.textBox4.TabIndex = 15;
+            this.textBox4.Text = "3";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox5.Location = new System.Drawing.Point(360, 415);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(59, 20);
+            this.textBox5.TabIndex = 17;
+            this.textBox5.Text = "250";
+            // 
+            // label9
+            // 
+            this.label9.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(245, 419);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(109, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "projectile velocity m/s";
+            // 
+            // label10
+            // 
+            this.label10.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(110, 388);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(61, 13);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "Arrival time:";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // label11
+            // 
+            this.label11.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(748, 9);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(40, 13);
+            this.label11.TabIndex = 20;
+            this.label11.Text = "Point 1";
+            // 
+            // label12
+            // 
+            this.label12.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label12.AutoSize = true;
+            this.label12.BackColor = System.Drawing.Color.Fuchsia;
+            this.label12.Location = new System.Drawing.Point(200, 388);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(47, 13);
+            this.label12.TabIndex = 21;
+            this.label12.Text = "Azimuth:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+    }
+}
+
---
+++ ArtCalculator/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Reflection.Emit;
+using System.Windows.Forms;
+using static System.Windows.Forms.LinkLabel;
+
+namespace ArtCalculator
+{
+    public partial class Form1 : Form
+    {
+        private Rectangle bounds;
+        private Bitmap screenshot;
+
+        private Point firstClick = Point.Empty; // Первый клик
+        private Point secondClick = Point.Empty; // Второй клик
+
+        private double gridDist;
+
+        private double targetDistance;
+
+        private void Form1_Load(object sender, EventArgs e){}
+        private void label2_Click(object sender, EventArgs e){}
+        private void label6_Click(object sender, EventArgs e) { }
+        private void label10_Click(object sender, EventArgs e) { }
+
+        public Form1()
+        {
+            InitializeComponent();
+            UpdateViewPointStatus();
+            label1.BackColor = Color.Red;
+        }
+
+        private void button1_Click(object sender, EventArgs e) // Make Screen Shot
+        {
+            bounds = Screen.PrimaryScreen.Bounds;
+
+            int right = bounds.Width; 
+            int bottom = bounds.Height; 
+            int left = right - (bounds.Width / int.Parse(textBox3.Text)); 
+            int top = bottom - (bounds.Height / int.Parse(textBox1.Text)); 
+
+            // Создайте Bitmap для хранения скриншота
+            screenshot = new Bitmap(bounds.Width / int.Parse(textBox4.Text), bounds.Height / int.Parse(textBox4.Text));
+
+            using (Graphics graphics = Graphics.FromImage(screenshot))
+            {
+                graphics.CopyFromScreen(left, top, 0, 0, screenshot.Size);
+            }
+
+            pictureBox1.Image = screenshot;
+        }
+        private void pictureBox1_Click(object sender, EventArgs e) // Screen Shot IMG
+        {
+            MouseEventArgs me = (MouseEventArgs)e;
+            if (firstClick == Point.Empty)
+            {
+                firstClick = me.Location;
+            }
+            else if (secondClick == Point.Empty)
+            {
+                secondClick = me.Location;
+            }
+            if (firstClick != Point.Empty & secondClick != Point.Empty)
+            {
+                if (gridDist == 0)
+                {
+                    WriteGridLine();
+                }
+                else
+                {
+                    WriteDistanceLine();
+                }
+
+                firstClick = Point.Empty;
+                secondClick = Point.Empty;
+            }
+            UpdateViewPointStatus();
+        }
+        private void WriteGridLine()
+        {
+            using (Graphics g = pictureBox1.CreateGraphics())
+            {
+                Pen pen = new Pen(Color.Green, 2); 
+                g.DrawLine(pen, firstClick, secondClick);
+            }
+            using (Graphics g = pictureBox1.CreateGraphics())
+            {
+                Font font = new Font("Arial", 10);
+                SolidBrush brush = new SolidBrush(Color.Green);
+                g.DrawString("Grid Size", font, brush, secondClick.X + 5, secondClick.Y + 5); 
+            }
+            gridDist = CalculateDistance(firstClick, secondClick);
+            label1.Text = "Grid Size: " + Math.Round(gridDist).ToString();
+            label1.BackColor = Color.Green;
+        }
+        private void WriteDistanceLine()
+        {
+            Color lineColor = GetRandomColor();
+
+            using (Graphics g = pictureBox1.CreateGraphics())
+            {
+                Pen pen = new Pen(lineColor, 2); 
+                g.DrawLine(pen, firstClick, secondClick);
+            }
+
+            targetDistance = CalculateDistance(firstClick, secondClick);
+            label3.Text = "Line Distance: " + Math.Round(targetDistance).ToString();
+
+            if (textBox2.Text != "")
+            {
+                label5.Text = Math.Round(CalculateDistanceInGame(Double.Parse(textBox2.Text), gridDist, targetDistance)).ToString() + "m";
+            }
+            if (textBox5.Text != "")
+            {
+                label10.Text = "Arrival time: " + (CalculateDistanceInGame(Double.Parse(textBox2.Text), gridDist, targetDistance) / Double.Parse(textBox5.Text)).ToString("F2") + "s";
+            }
+            label12.Text = "Azimuth: " + CalculateAzimuth(firstClick, secondClick).ToString("F2");
+
+            using (Graphics g = pictureBox1.CreateGraphics())
+            {
+                Font font = new Font("Arial", 10);
+                SolidBrush blackBrush = new SolidBrush(Color.Black);
+                SolidBrush textBrush = new SolidBrush(IncreaseBrightness(lineColor, 100)); 
+
+                SizeF textSize = g.MeasureString($"{label5.Text}\n{label12.Text}", font);
+
+                float rectangleWidth = textSize.Width + 10;
+                float rectangleHeight = textSize.Height + 10;
+                float rectangleX = secondClick.X + 5; 
+                float rectangleY = secondClick.Y + 5; 
+
+                g.FillRectangle(blackBrush, rectangleX, rectangleY, rectangleWidth, rectangleHeight);
+
+                g.DrawString($"{label5.Text}\n{label12.Text}", font, textBrush, rectangleX + 5, rectangleY + 5); 
+
+                font.Dispose();
+                blackBrush.Dispose();
+                textBrush.Dispose();
+            }
+
+        }
+        private double CalculateDistance(Point p1, Point p2)
+        {
+            int dx = p2.X - p1.X;
+            int dy = p2.Y - p1.Y;
+            return Math.Sqrt(dx * dx + dy * dy);
+        }
+        private double CalculateAzimuth(Point p1, Point p2)
+        {
+            int dx = p2.X - p1.X;
+            int dy = p2.Y - p1.Y;
+
+            double radians = Math.Atan2(dy, dx);
+
+            double degrees = radians * (180 / Math.PI);
+            degrees = (degrees + 360) % 360; 
+
+            degrees += 90;
+            degrees %= 360;
+
+            return degrees;
+        }
+
+
+        private void button2_Click(object sender, EventArgs e) // Set Grid
+        {
+            gridDist = 0;
+            label1.BackColor = Color.Red;
+            label1.Text = "Grid Size: ";
+            firstClick = Point.Empty; 
+            secondClick = Point.Empty; 
+            pictureBox1.Invalidate();
+        }
+
+        private double CalculateDistanceInGame(double gameGridSize, double gridDist, double targetDistance)
+        {
+            return (gameGridSize / gridDist) * targetDistance;
+        }
+
+        private void button3_Click(object sender, EventArgs e) // Clear Lines
+        {
+            pictureBox1.Invalidate();
+        }    
+        private void UpdateViewPointStatus()
+        {
+            if (firstClick == Point.Empty)
+                label11.BackColor = Color.Red;
+            else
+                label11.BackColor = Color.Green;
+        }
+        private Color GetRandomColor()
+        {
+            Random random = new Random();
+            int red = random.Next(256); 
+            int green = random.Next(256);
+            int blue = random.Next(256);
+            Color randomColor = Color.FromArgb(red, green, blue);
+            return randomColor;
+        }
+        private Color IncreaseBrightness(Color color, int increment)
+        {
+            int red = Math.Min(color.R + increment, 255);
+            int green = Math.Min(color.G + increment, 255);
+            int blue = Math.Min(color.B + increment, 255);
+            return Color.FromArgb(red, green, blue);
+        }
+    }
+}
